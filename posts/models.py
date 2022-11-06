8d4f9b692b0dbdd1324e58b6bdf80058e041c624
carrie <nwcarri80@gmail.com>
2022-11-06 17:28:41 +0300

    add patient models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(blank=True, null=True)
+    dob = models.DateField()
+    idNo = models.IntegerField(blank=True, null=True)
+    phone_number = models.IntegerField()
+    joined_at = models.DateTimeField(auto_now_add=True)    
+
+    class Meta:
+        ordering = ['-joined_at']
+
+    def __str__(self):
+        return str(self.first_name)
