f9be916e270ab474cf1765f37e812ac59d014853
carrie <nwcarri80@gmail.com>
2022-11-06 17:30:20 +0300

    add all pages views

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from .models import Patient
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    title = 'Home'
+    return render(request, 'index.html', {title:title})
+
+
+def patients(request):
+    title = 'Patients'
+    patients = Patient.objects.all()
+    print(patients)
+    return render(request, 'patients.html', {'title':title, 'patients':patients})    
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('Home')
